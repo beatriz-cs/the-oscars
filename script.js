f98ae689ffeead1e527ee48beef5e1b3d3af6ab4
Beatriz Cardeal (BCS) <106784551+beatriz-cs@users.noreply.github.com>
2022-07-16 15:20:38 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+let comedy = [
+    {
+        title: "White Chicks",
+        director: "Keenan Ivory Wayans",
+        year: 2004,
+    },
+    {
+        title: "We're the Millers",
+        director: "Rawson Marshall Thurber",
+        year: 2013, 
+    },
+    {
+        title: "A Dog's Will",
+        director: "Gael Arraes",
+        year: 2000, 
+    }
+]
+
+console.log(comedy);
+
+let action = [
+    {
+        title: "Elite Squad",
+        director: "José Padilha",
+        year: 2007,
+    },
+    {
+        title: "City of God",
+        director: "Fernando Meirelles and Kátia Lund",
+        year: 2002,
+    },
+    {
+        title: "Birds of Prey",
+        director: "Cathy Yan",
+        year: 2020,
+    }
+]
+
+console.log(action);
+
+let fantasy = [
+    {
+        title: "Corpse Bride",
+        director: "Tim Burton",
+        year: 2005,
+    },
+    {
+        title: "Pan's Labyrinth",
+        director: "Guillermo del Toro",
+        year: 2006,
+    },
+    {
+        title: "Jurassic Park",
+        director: "Steven Spielberg",
+        year: 1993,
+    }
+]
+
+console.log(fantasy);
